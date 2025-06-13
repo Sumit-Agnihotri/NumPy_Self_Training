128b7e1b0fa6992f3b4b496f4c158ba33ac28a3c
Sumit Agnihotri <163757254+Sumit-Agnihotri@users.noreply.github.com>
2025-06-13 07:31:57 +0530

    NumPy

---
+++ Interview Questions/Question 1.py
@@ -0,0 +1,8 @@
+# What is NumPy?
+
+'''
+NumPy is a powerful open-source library in Python that provides
+1. Support for large, multi-dimensional arrays and matrices
+2. A collection of mathematical functions to operate on these arrays
+3. It is widely used in data science, machine learning, and scientific computing.
+'''
